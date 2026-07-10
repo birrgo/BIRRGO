8c3d45775de41cb546a075a444fc1c835361c3d4
birrgo <picuuae1444@gmail.com>
2026-07-10 20:04:18 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11266752
+    "domain": "5gvci.com",
+    "zoneId": 11267042
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
